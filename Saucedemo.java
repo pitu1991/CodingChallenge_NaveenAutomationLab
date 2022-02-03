22462e936dcc5f7730886dbf6681be8b049d50a1
pitu1991 <shyamsundar.sundar@gmail.com>
2022-02-03 10:45:27 +0530

    Update Saucedemo.java
    Removed for loop for addToCart element

--- Saucedemo.java
+++ Saucedemo.java
@@ -21,6 +21,7 @@ public class Saucedemo {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
 	}
 
 	@Test(priority = 1)
@@ -28,35 +29,35 @@ public class Saucedemo {
 
 		WebElement userName = driver.findElement(By.id("user-name"));
 		WebElement password = driver.findElement(By.id("password"));
-		WebElement loginButton = driver.findElement(By.id("login-button"));
-		userName.click();
-		userName.sendKeys("standard_user");
-		password.click();
-		password.sendKeys("secret_sauce");
+		WebElement loginButton = driver.findElement(By.id("login-button"));		
+		enterText(userName,"standard_user");
+		enterText(password,"secret_sauce");
 		loginButton.click();
-
 	}
 
 	@Test(priority = 2)
 	public void addFunction() {
 		List<Double> priceOfEach = new ArrayList<Double>();
-		List<WebElement> addcart = new ArrayList<WebElement>();
 		List<WebElement> priceAmount = driver.findElements(By.xpath("//div[@class='inventory_item_price']"));
 		List<WebElement> addToCart = driver.findElements(By.xpath("//*[@class='inventory_item']//descendant::button"));
 
 		for (WebElement i : priceAmount) {
 			priceOfEach.add(Double.valueOf(i.getText().replace("$", "")));
 		}
-		for (WebElement j : addToCart) {
-			addcart.add(j);
-		}
+
 		
 		Double Max_val = Collections.max(priceOfEach, null);
 
 		int pos = priceOfEach.indexOf(Max_val);
 
-		addcart.get(pos).click();
+		addToCart.get(pos).click();
 
+	}
+	
+	public void enterText(WebElement text,String name) {
+		text.click();
+		text.clear();
+		text.sendKeys(name);
 	}
 
 }
